2ec18d4feee29a5191faf7aaa33be00a0f43154d
já už nevim <149662501+FMatyas@users.noreply.github.com>
2026-02-11 22:36:17 +0100

    Refactor Zlomek class constructors and methods

--- zlomek_proverka/zlomek.cpp
+++ zlomek_proverka/zlomek.cpp
@@ -1,42 +1,57 @@
 #include "zlomek.h"
-#include <iostream>
 
-// Konstruktor bez parametrů (Bod 2)
+// Bod 2: Konstruktory
 Zlomek::Zlomek() {
     citatel = 1;
     jmenovatel = 1;
 }
 
-// Konstruktor s parametry a kontrolou (Bod 2)
 Zlomek::Zlomek(int c, int j) {
     citatel = c;
-    if (j == 0) jmenovatel = 1;
-    else jmenovatel = j;
+    if (j == 0) {
+        jmenovatel = 1;
+    } else {
+        jmenovatel = j;
+    }
 }
 
 Zlomek::~Zlomek() {}
 
-// Getters a Setters (Bod 3)
-void Zlomek::setCitatel(int c) { citatel = c; }
-void Zlomek::setJmenovatel(int j) { if (j != 0) jmenovatel = j; }
-int Zlomek::getCitatel() const { return citatel; }
-int Zlomek::getJmenovatel() const { return jmenovatel; }
-
-// Výpis zlomku (Bod 3)
-void Zlomek::vypis() const {
-    std::cout << citatel << "/" << jmenovatel;
+// --- ROZEPSANÉ SETTERY ---
+void Zlomek::setCitatel(int c) {
+    citatel = c;
 }
 
-// Převod na desetinné číslo (Bod 4)
+void Zlomek::setJmenovatel(int j) {
+    if (j != 0) {
+        jmenovatel = j;
+    }
+}
+
+// --- ROZEPSANÉ GETTERY ---
+int Zlomek::getCitatel() const {
+    return citatel;
+}
+
+int Zlomek::getJmenovatel() const {
+    return jmenovatel;
+}
+
+// Bod 4: Ostatní metody
 double Zlomek::naDesetinne() const {
     return (double)citatel / jmenovatel;
 }
 
-// Převrácení zlomku pomocí swapu (Bod 4)
 void Zlomek::prevrat() {
     if (citatel != 0) {
         int pom = citatel;
         citatel = jmenovatel;
         jmenovatel = pom;
     }
+}
+
+// Bod 3: Výpis přes operátor <<
+std::ostream& operator<<(std::ostream& os, const Zlomek& z) {
+    os << z.citatel << "/" << z.jmenovatel;
+    return os;
 }
